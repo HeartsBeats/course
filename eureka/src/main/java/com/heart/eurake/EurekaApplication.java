5d4de63125b3be6c6ac545b9290e69492aaca24f
Hearts <1127957114@qq.com>
2020-10-16 23:34:16 +0800

    1.增加eureka子模块作为注册服务器 2.将单maven项目改造成maven父子项目

--- eureka/src/main/java/com/heart/eurake/EurekaApplication.java
+++ eureka/src/main/java/com/heart/eurake/EurekaApplication.java
@@ -1,13 +1,13 @@
-package com.heart.course;
+package com.heart.eurake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CourseApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CourseApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
