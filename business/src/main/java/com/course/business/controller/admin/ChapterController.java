6c496c63b9da926f4b2ac92d696d6821710bf979
Hearts <1127957114@qq.com>
2020-10-24 22:43:12 +0800

    1. 增加dto层，用于controller和service层 2. 增加ChapterDto 3. 修改ChapterService，将返回Chapter改成返回ChapterDto

--- business/src/main/java/com/course/business/controller/admin/ChapterController.java
+++ business/src/main/java/com/course/business/controller/admin/ChapterController.java
@@ -1,6 +1,6 @@
-package com.course.business.controller;
+package com.course.business.controller.admin;
 
-import com.course.server.domain.Chapter;
+import com.course.server.dto.ChapterDto;
 import com.course.server.service.ChapterService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,15 +9,18 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @RestController
+@RequestMapping("/admin")
 public class ChapterController {
     @Resource
     private ChapterService chapterService;
+
     @RequestMapping("/hello")
     public String helloWorld() {
         return "success";
     }
+
     @RequestMapping("/chapter")
-    public List<Chapter> chapter() {
+    public List<ChapterDto> chapter() {
         return chapterService.list();
     }
 
--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -2,10 +2,14 @@ package com.course.server.service;
 
 
 import com.course.server.domain.Chapter;
+import com.course.server.domain.ChapterExample;
+import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,8 +17,16 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
 
-    public List<Chapter> list() {
-        return chapterMapper.selectByExample(null);
+    public List<ChapterDto> list() {
+        ChapterExample chapterExample = new ChapterExample();
+        List<Chapter> chapters = chapterMapper.selectByExample(chapterExample);
+        List<ChapterDto> chapterDtoList  = new ArrayList<>();
+        for (int i = 0; i < chapters.size(); i++) {
+            Chapter chapter = chapters.get(i);
+            ChapterDto chapterDto = new ChapterDto();
+            BeanUtils.copyProperties(chapter,chapterDto);
+            chapterDtoList.add(chapterDto);
+        }
+        return chapterDtoList;
     }
-
 }
