761d4c47c4b778f16a2930f377133ecfffa49b20
Hearts <1127957114@qq.com>
2020-10-24 21:48:59 +0800

    1.创建business模块

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,3 +1,15 @@
+drop table if exists `chapter`;
+create table `chapter`
+(
+    `id`        char(8) not null comment 'ID',
+    `course_id` char(8) comment '课程ID',
+    `name`      varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='大章';
+
+
+-- -------------------------测试
 drop table if exists `test`;
 create table `test` (
   `id` char(8) not null default '' comment 'id',
