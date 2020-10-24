a00982ee884efc461f82dbb12b3df98c15afadaa
Hearts <1127957114@qq.com>
2020-10-24 10:45:39 +0800

    1. 解决由于index页面静态资源引入代码错误页面报错：$ is not defined的问题

--- admin/src/App.vue
+++ admin/src/App.vue
@@ -1,10 +1,6 @@
 <template>
-
+  <div id="app">
+    <router-view/>
+  </div>
 </template>
-
-<script>
-export default {
-  name: 'app',
-}
-</script>
 
--- admin/src/main.js
+++ admin/src/main.js
@@ -1,5 +1,5 @@
 import Vue from 'vue'
-import App from './app.vue'
+import App from './App'
 import router from './router'
 
 Vue.config.productionTip = false;
