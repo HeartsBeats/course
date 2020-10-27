5c61e5547da0d5312d12f9070d8bd13c82b40f0a
Hearts <1127957114@qq.com>
2020-10-27 08:40:27 +0800

    1. 增加新增大章功能，前端代码开发 2. eslint关闭no-unused-vars校验

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
     <p>
-      <button v-on:click="list()" class="btn btn-white btn-default btn-round">
+      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-edit"></i>
+        新增
+      </button>
+      &nbsp;
+      <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         刷新
       </button>
     </p>
+    <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
   <table id="simple-table" class="table  table-bordered table-hover">
     <thead>
     <tr>
@@ -77,12 +83,45 @@
     </tr>
     </tbody>
   </table>
+    <div class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span
+                aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">表单</h4>
+          </div>
+          <div class="modal-body">
+            <form class="form-horizontal">
+              <div class="form-group">
+                <label class="col-sm-2 control-label">名称</label>
+                <div class="col-sm-10">
+                  <input class="form-control" placeholder="名称">
+                </div>
+              </div>
+              <div class="form-group">
+                <label class="col-sm-2 control-label">课程ID</label>
+                <div class="col-sm-10">
+                  <input class="form-control" placeholder="课程ID">
+                </div>
+              </div>
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button type="button" class="btn btn-primary">保存</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
   </div>
 </template>
 
 <script>
+import Pagination from "../../components/pagination";
 export default {
   name: "chapter",
+  components: {Pagination},
   data: function () {
     return {
       chapters: []
@@ -90,18 +129,24 @@ export default {
   },
   mounted: function () {
     let _this = this;
-    _this.list();
+    _this.$refs.pagination.size = 5;
+    _this.list(1);
     // this.$parent.activeSidebar("business-chapter-sidebar")
   },
   methods: {
-    list() {
+    add() {
+      let _this = this;
+      $(".modal").modal("show");
+    },
+    list(page) {
       let _this = this;
       _this.$ajax.post('http://127.0.0.1:9000/business/admin/chapter/list',{
-        page: 1,
-        size: 2
+        page: page,
+        size: _this.$refs.pagination.size,
       }).then((response) => {
         console.log("查询大章列表结果：", response);
         _this.chapters = response.data.list;
+        _this.$refs.pagination.render(page, response.data.total);
       })
     }
   }
