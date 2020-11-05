8f7223fa9691f3d70fee4f38c5d2d228c5300a12
Hearts <1127957114@qq.com>
2020-11-05 20:27:46 +0800

    1. sessionStorage的key改成常量

--- admin/src/views/admin/chapter.vue
+++ admin/src/views/admin/chapter.vue
@@ -137,7 +137,7 @@ export default {
   mounted: function () {
     let _this = this;
     _this.$refs.pagination.size = 5;
-    let course = SessionStorage.get("course") || {};
+    let course = SessionStorage.get(SESSION_KEY_COURSE) || {};
     if (Tool.isEmpty(course)) {
       _this.$router.push("/welcome");
     }
@@ -226,7 +226,7 @@ export default {
      */
     toSection(chapter) {
       let _this = this;
-      SessionStorage.set("chapter", chapter);
+      SessionStorage.set(SESSION_KEY_CHAPTER, chapter);
       _this.$router.push("/business/section");
     }
   }
