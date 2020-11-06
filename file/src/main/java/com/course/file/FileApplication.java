368de7ed5e15cf4d41125d3952498e09da47c0bc
Hearts <1127957114@qq.com>
2020-11-06 18:48:33 +0800

    1.创建file模块,用于文件上传和存储

---
+++ file/src/main/java/com/course/file/FileApplication.java
@@ -0,0 +1,24 @@
+package com.course.file;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class FileApplication {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FileApplication.class);
+
+
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(FileApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("启动成功！！");
+        LOG.info("File地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+}
