e66395c4a6fbbbe720bc510b3712a47d671d7f7d
Hearts <1127957114@qq.com>
2020-11-09 21:33:13 +0800

    1. 文件上传功能开发：去掉文件管理的新增、修改、删除功能

--- file/src/main/java/com/course/file/controller/admin/FileController.java
+++ file/src/main/java/com/course/file/controller/admin/FileController.java
@@ -1,13 +1,14 @@
 package com.course.file.controller.admin;
 
-import com.course.server.dto.FileDto;
 import com.course.server.dto.PageDto;
 import com.course.server.dto.ResponseDto;
 import com.course.server.service.FileService;
-import com.course.server.util.ValidatorUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
 
@@ -29,34 +30,6 @@ public class FileController {
         ResponseDto responseDto = new ResponseDto();
         fileService.list(pageDto);
         responseDto.setContent(pageDto);
-        return responseDto;
-    }
-
-    /**
-    * 保存，id有值时更新，无值时新增
-    */
-    @PostMapping("/save")
-    public ResponseDto save(@RequestBody FileDto fileDto) {
-    // 保存校验
-                ValidatorUtil.require(fileDto.getId(), "id");
-                ValidatorUtil.require(fileDto.getPath(), "相对路径");
-                ValidatorUtil.length(fileDto.getPath(), "相对路径", 1, 100);
-                ValidatorUtil.length(fileDto.getName(), "文件名", 1, 100);
-                ValidatorUtil.length(fileDto.getSuffix(), "后缀", 1, 10);
-
-        ResponseDto responseDto = new ResponseDto();
-        fileService.save(fileDto);
-        responseDto.setContent(fileDto);
-        return responseDto;
-    }
-
-    /**
-    * 删除
-    */
-    @DeleteMapping("/delete/{id}")
-        public ResponseDto delete(@PathVariable String id) {
-        ResponseDto responseDto = new ResponseDto();
-        fileService.delete(id);
         return responseDto;
     }
 }
