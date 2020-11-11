1c1c9564aebdebd34219405965d60bbc207d3f3f
Hearts <1127957114@qq.com>
2020-11-11 12:07:22 +0800

    fixup! 1. 大文件断点续传与极速秒传：文件表增加分片相关字段

--- generator/src/main/java/com/course/generator/util/Field.java
+++ generator/src/main/java/com/course/generator/util/Field.java
@@ -1,17 +1,55 @@
 package com.course.generator.util;
+/**
+ *
+ * @Author: YJQ
+ * @Date: 2020-11-11 11:53
+ */
 
 public class Field {
-    private String name; // 字段名：course_id
-    private String nameHump; // 字段名小驼峰：courseId
-    private String nameBigHump; // 字段名大驼峰：CourseId
-    private String nameCn; // 中文名：课程
-    private String type; // 字段类型：char(8)
-    private String javaType; // java类型：String
-    private String comment; // 注释：课程|ID
-    private Boolean nullAble; // 是否可为空
-    private Integer length; // 字符串长度
-    private Boolean enums; // 是否是枚举
-    private String enumsConst; // 枚举常量 COURSE_LEVEL
+    /**
+        字段名：course_id
+     */
+    private String name;
+    /**
+     * 字段名小驼峰：courseId
+     */
+    private String nameHump;
+    /**
+     *字段名大驼峰：CourseId
+     */
+    private String nameBigHump;
+    /**
+     *  中文名：课程
+     */
+    private String nameCn;
+    /**
+     * 字段类型：char(8)
+     */
+    private String type;
+    /**
+     * java类型：String
+     */
+    private String javaType;
+    /**
+     * 注释：课程|ID
+     */
+    private String comment;
+    /**
+     *  是否可为空
+     */
+    private Boolean nullAble;
+    /**
+     * 字符串长度
+     */
+    private Integer length;
+    /**
+     *  是否是枚举
+     */
+    private Boolean enums;
+    /**
+     *  枚举常量 COURSE_LEVEL
+     */
+    private String enumsConst;
 
     public Boolean getEnums() {
         return enums;
