f5d82767b721af66aa831a3434e20c0489ed60eb
Hearts <1127957114@qq.com>
2020-11-13 08:34:02 +0800

    1. 大文件断点续传与极速秒传：设置分片文件名，以分片索引结尾

--- admin/src/components/big-file.vue
+++ admin/src/components/big-file.vue
@@ -78,12 +78,13 @@ export default {
       }
 
       // 文件分片
-      let shardSize = 50 * 1024 * 1024;    //以20MB为一个分片
-      let shardIndex = 0;		//分片索引
-      let start = shardIndex * shardSize;	//当前分片起始位置
-      let end = Math.min(file.size, start + shardSize); //当前分片结束位置
-      let fileShard = file.slice(start, end); //从文件中截取当前的分片数据
       let size = file.size;
+      let use = _this.use;
+      let shardSize = 50 * 1024 * 1024;    //以20MB为一个分片
+      let shardIndex = 1;		//分片索引
+      let start = (shardIndex - 1) * shardSize;	//当前分片起始位置
+      let end = Math.min(size, start + shardSize); //当前分片结束位置
+      let fileShard = file.slice(start, end); //从文件中截取当前的分片数据
       let shardTotal = Math.ceil(size / shardSize); //总片数
 
       // // key："shard"必须和后端controller参数名一致
@@ -108,17 +109,17 @@ export default {
       let fileReader = new FileReader();
       fileReader.onload = function(e) {
         let base64 = e.target.result;
-        console.log("base64:", base64);
+        // console.log("base64:", base64);
 
         let param = {
           'shard': base64,
           'shardIndex': shardIndex,
           'shardSize': shardSize,
           'shardTotal': shardTotal,
-          'use': _this.use,
-          'name': file.name,
+          'use': use,
+          'name': fileName,
           'suffix': suffix,
-          'size': file.size,
+          'size': size,
           'key': key62
         };
 
