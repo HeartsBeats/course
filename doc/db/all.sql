b3b636c55e93964a67ef568de1ae3be76bd0f843
Hearts <1127957114@qq.com>
2020-11-16 21:43:51 +0800

    1. 视频加密与授权播放：file表增加vod字段

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,26 +1,31 @@
 -- 课程
 drop table if exists course;
-create table course (
-                        id char(8) not null default '' comment 'id',
-                        name varchar(50) not null comment '名称',
-                        summary varchar(2000) comment '概述',
-                        time int default 0 comment '时长|单位秒',
-                        price decimal(8,2) default 0.00 comment '价格（元）',
-                        image varchar(100) comment '封面',
-                        level char(1) comment '级别|枚举[CourseLevelEnum]：ONE("1", "初级"),TWO("2", "中级"),THREE("3", "高级")',
-                        charge char(1) comment '收费|枚举[CourseChargeEnum]：CHARGE("C", "收费"),FREE("F", "免费")',
-                        status char(1) comment '状态|枚举[CourseStatusEnum]：PUBLISH("P", "发布"),DRAFT("D", "草稿")',
-                        enroll integer default 0 comment '报名数',
-                        sort int comment '顺序',
-                        created_at datetime(3) comment '创建时间',
-                        updated_at datetime(3) comment '修改时间',
-                        primary key (id)
-) engine=innodb default charset=utf8mb4 comment='课程';
+create table course
+(
+    id         char(8)     not null default '' comment 'id',
+    name       varchar(50) not null comment '名称',
+    summary    varchar(2000) comment '概述',
+    time       int                  default 0 comment '时长|单位秒',
+    price      decimal(8, 2)        default 0.00 comment '价格（元）',
+    image      varchar(100) comment '封面',
+    level      char(1) comment '级别|枚举[CourseLevelEnum]：ONE("1", "初级"),TWO("2", "中级"),THREE("3", "高级")',
+    charge     char(1) comment '收费|枚举[CourseChargeEnum]：CHARGE("C", "收费"),FREE("F", "免费")',
+    status     char(1) comment '状态|枚举[CourseStatusEnum]：PUBLISH("P", "发布"),DRAFT("D", "草稿")',
+    enroll     integer              default 0 comment '报名数',
+    sort       int comment '顺序',
+    created_at datetime(3) comment '创建时间',
+    updated_at datetime(3) comment '修改时间',
+    primary key (id)
+) engine = innodb
+  default charset = utf8mb4 comment ='课程';
 
 INSERT INTO course (id, name, summary, time, price, image, level, charge, status, enroll, sort, created_at, updated_at)
 VALUES ('00000001', '测试课程01', '这是一门测试课程', 7200, 19.9, '', 1, 'C', 'D', 100, 0, now(), now());
-update course set teacher_id = '00000001' where id = '00000001';
-alter table `course` add column (`teacher_id` char(8) comment '讲师|teacher.id');
+update course
+set teacher_id = '00000001'
+where id = '00000001';
+alter table `course`
+    add column (`teacher_id` char(8) comment '讲师|teacher.id');
 
 #  大章
 drop table if exists `chapter`;
@@ -33,20 +38,34 @@ create table `chapter`
 ) engine = innodb
   default charset = utf8mb4 comment ='大章';
 
-insert into `chapter` (id, course_id, name) values ('00000001', '00000000', '测试大章01');
-insert into `chapter` (id, course_id, name) values ('00000002', '00000000', '测试大章02');
-insert into `chapter` (id, course_id, name) values ('00000003', '00000000', '测试大章03');
-insert into `chapter` (id, course_id, name) values ('00000004', '00000000', '测试大章04');
-insert into `chapter` (id, course_id, name) values ('00000005', '00000000', '测试大章05');
-insert into `chapter` (id, course_id, name) values ('00000006', '00000000', '测试大章06');
-insert into `chapter` (id, course_id, name) values ('00000007', '00000000', '测试大章07');
-insert into `chapter` (id, course_id, name) values ('00000008', '00000000', '测试大章08');
-insert into `chapter` (id, course_id, name) values ('00000009', '00000000', '测试大章09');
-insert into `chapter` (id, course_id, name) values ('00000010', '00000000', '测试大章10');
-insert into `chapter` (id, course_id, name) values ('00000011', '00000000', '测试大章11');
-insert into `chapter` (id, course_id, name) values ('00000012', '00000000', '测试大章12');
-insert into `chapter` (id, course_id, name) values ('00000013', '00000000', '测试大章13');
-insert into `chapter` (id, course_id, name) values ('00000014', '00000000', '测试大章14');
+insert into `chapter` (id, course_id, name)
+values ('00000001', '00000000', '测试大章01');
+insert into `chapter` (id, course_id, name)
+values ('00000002', '00000000', '测试大章02');
+insert into `chapter` (id, course_id, name)
+values ('00000003', '00000000', '测试大章03');
+insert into `chapter` (id, course_id, name)
+values ('00000004', '00000000', '测试大章04');
+insert into `chapter` (id, course_id, name)
+values ('00000005', '00000000', '测试大章05');
+insert into `chapter` (id, course_id, name)
+values ('00000006', '00000000', '测试大章06');
+insert into `chapter` (id, course_id, name)
+values ('00000007', '00000000', '测试大章07');
+insert into `chapter` (id, course_id, name)
+values ('00000008', '00000000', '测试大章08');
+insert into `chapter` (id, course_id, name)
+values ('00000009', '00000000', '测试大章09');
+insert into `chapter` (id, course_id, name)
+values ('00000010', '00000000', '测试大章10');
+insert into `chapter` (id, course_id, name)
+values ('00000011', '00000000', '测试大章11');
+insert into `chapter` (id, course_id, name)
+values ('00000012', '00000000', '测试大章12');
+insert into `chapter` (id, course_id, name)
+values ('00000013', '00000000', '测试大章13');
+insert into `chapter` (id, course_id, name)
+values ('00000014', '00000000', '测试大章14');
 -- 小节
 DROP TABLE IF EXISTS `section`;
 CREATE TABLE `section`
@@ -85,52 +104,98 @@ create table `category`
 ) engine = innodb
   default charset = utf8mb4 comment ='分类';
 
-insert into `category` (id, parent, name, sort) values ('00000100', '00000000', '前端技术', 100);
-insert into `category` (id, parent, name, sort) values ('00000101', '00000100', 'html/css', 101);
-insert into `category` (id, parent, name, sort) values ('00000102', '00000100', 'javascript', 102);
-insert into `category` (id, parent, name, sort) values ('00000103', '00000100', 'vue.js', 103);
-insert into `category` (id, parent, name, sort) values ('00000104', '00000100', 'react.js', 104);
-insert into `category` (id, parent, name, sort) values ('00000105', '00000100', 'angular', 105);
-insert into `category` (id, parent, name, sort) values ('00000106', '00000100', 'node.js', 106);
-insert into `category` (id, parent, name, sort) values ('00000107', '00000100', 'jquery', 107);
-insert into `category` (id, parent, name, sort) values ('00000108', '00000100', '小程序', 108);
-insert into `category` (id, parent, name, sort) values ('00000200', '00000000', '后端技术', 200);
-insert into `category` (id, parent, name, sort) values ('00000201', '00000200', 'java', 201);
-insert into `category` (id, parent, name, sort) values ('00000202', '00000200', 'springboot', 202);
-insert into `category` (id, parent, name, sort) values ('00000203', '00000200', 'spring cloud', 203);
-insert into `category` (id, parent, name, sort) values ('00000204', '00000200', 'ssm', 204);
-insert into `category` (id, parent, name, sort) values ('00000205', '00000200', 'python', 205);
-insert into `category` (id, parent, name, sort) values ('00000206', '00000200', '爬虫', 206);
-insert into `category` (id, parent, name, sort) values ('00000300', '00000000', '移动开发', 300);
-insert into `category` (id, parent, name, sort) values ('00000301', '00000300', 'android', 301);
-insert into `category` (id, parent, name, sort) values ('00000302', '00000300', 'ios', 302);
-insert into `category` (id, parent, name, sort) values ('00000303', '00000300', 'react native', 303);
-insert into `category` (id, parent, name, sort) values ('00000304', '00000300', 'ionic', 304);
-insert into `category` (id, parent, name, sort) values ('00000400', '00000000', '前沿技术', 400);
-insert into `category` (id, parent, name, sort) values ('00000401', '00000400', '微服务', 401);
-insert into `category` (id, parent, name, sort) values ('00000402', '00000400', '区块链', 402);
-insert into `category` (id, parent, name, sort) values ('00000403', '00000400', '机器学习', 403);
-insert into `category` (id, parent, name, sort) values ('00000404', '00000400', '深度学习', 404);
-insert into `category` (id, parent, name, sort) values ('00000405', '00000400', '数据分析&挖掘', 405);
-insert into `category` (id, parent, name, sort) values ('00000500', '00000000', '云计算&大数据', 500);
-insert into `category` (id, parent, name, sort) values ('00000501', '00000500', '大数据', 501);
-insert into `category` (id, parent, name, sort) values ('00000502', '00000500', 'hadoop', 502);
-insert into `category` (id, parent, name, sort) values ('00000503', '00000500', 'spark', 503);
-insert into `category` (id, parent, name, sort) values ('00000504', '00000500', 'hbase', 504);
-insert into `category` (id, parent, name, sort) values ('00000505', '00000500', '阿里云', 505);
-insert into `category` (id, parent, name, sort) values ('00000506', '00000500', 'docker', 506);
-insert into `category` (id, parent, name, sort) values ('00000507', '00000500', 'kubernetes', 507);
-insert into `category` (id, parent, name, sort) values ('00000600', '00000000', '运维&测试', 600);
-insert into `category` (id, parent, name, sort) values ('00000601', '00000600', '运维', 601);
-insert into `category` (id, parent, name, sort) values ('00000602', '00000600', '自动化运维', 602);
-insert into `category` (id, parent, name, sort) values ('00000603', '00000600', '中间件', 603);
-insert into `category` (id, parent, name, sort) values ('00000604', '00000600', 'linux', 604);
-insert into `category` (id, parent, name, sort) values ('00000605', '00000600', '测试', 605);
-insert into `category` (id, parent, name, sort) values ('00000606', '00000600', '自动化测试', 606);
-insert into `category` (id, parent, name, sort) values ('00000700', '00000000', '数据库', 700);
-insert into `category` (id, parent, name, sort) values ('00000701', '00000700', 'mysql', 701);
-insert into `category` (id, parent, name, sort) values ('00000702', '00000700', 'redis', 702);
-insert into `category` (id, parent, name, sort) values ('00000703', '00000700', 'mongodb', 703);
+insert into `category` (id, parent, name, sort)
+values ('00000100', '00000000', '前端技术', 100);
+insert into `category` (id, parent, name, sort)
+values ('00000101', '00000100', 'html/css', 101);
+insert into `category` (id, parent, name, sort)
+values ('00000102', '00000100', 'javascript', 102);
+insert into `category` (id, parent, name, sort)
+values ('00000103', '00000100', 'vue.js', 103);
+insert into `category` (id, parent, name, sort)
+values ('00000104', '00000100', 'react.js', 104);
+insert into `category` (id, parent, name, sort)
+values ('00000105', '00000100', 'angular', 105);
+insert into `category` (id, parent, name, sort)
+values ('00000106', '00000100', 'node.js', 106);
+insert into `category` (id, parent, name, sort)
+values ('00000107', '00000100', 'jquery', 107);
+insert into `category` (id, parent, name, sort)
+values ('00000108', '00000100', '小程序', 108);
+insert into `category` (id, parent, name, sort)
+values ('00000200', '00000000', '后端技术', 200);
+insert into `category` (id, parent, name, sort)
+values ('00000201', '00000200', 'java', 201);
+insert into `category` (id, parent, name, sort)
+values ('00000202', '00000200', 'springboot', 202);
+insert into `category` (id, parent, name, sort)
+values ('00000203', '00000200', 'spring cloud', 203);
+insert into `category` (id, parent, name, sort)
+values ('00000204', '00000200', 'ssm', 204);
+insert into `category` (id, parent, name, sort)
+values ('00000205', '00000200', 'python', 205);
+insert into `category` (id, parent, name, sort)
+values ('00000206', '00000200', '爬虫', 206);
+insert into `category` (id, parent, name, sort)
+values ('00000300', '00000000', '移动开发', 300);
+insert into `category` (id, parent, name, sort)
+values ('00000301', '00000300', 'android', 301);
+insert into `category` (id, parent, name, sort)
+values ('00000302', '00000300', 'ios', 302);
+insert into `category` (id, parent, name, sort)
+values ('00000303', '00000300', 'react native', 303);
+insert into `category` (id, parent, name, sort)
+values ('00000304', '00000300', 'ionic', 304);
+insert into `category` (id, parent, name, sort)
+values ('00000400', '00000000', '前沿技术', 400);
+insert into `category` (id, parent, name, sort)
+values ('00000401', '00000400', '微服务', 401);
+insert into `category` (id, parent, name, sort)
+values ('00000402', '00000400', '区块链', 402);
+insert into `category` (id, parent, name, sort)
+values ('00000403', '00000400', '机器学习', 403);
+insert into `category` (id, parent, name, sort)
+values ('00000404', '00000400', '深度学习', 404);
+insert into `category` (id, parent, name, sort)
+values ('00000405', '00000400', '数据分析&挖掘', 405);
+insert into `category` (id, parent, name, sort)
+values ('00000500', '00000000', '云计算&大数据', 500);
+insert into `category` (id, parent, name, sort)
+values ('00000501', '00000500', '大数据', 501);
+insert into `category` (id, parent, name, sort)
+values ('00000502', '00000500', 'hadoop', 502);
+insert into `category` (id, parent, name, sort)
+values ('00000503', '00000500', 'spark', 503);
+insert into `category` (id, parent, name, sort)
+values ('00000504', '00000500', 'hbase', 504);
+insert into `category` (id, parent, name, sort)
+values ('00000505', '00000500', '阿里云', 505);
+insert into `category` (id, parent, name, sort)
+values ('00000506', '00000500', 'docker', 506);
+insert into `category` (id, parent, name, sort)
+values ('00000507', '00000500', 'kubernetes', 507);
+insert into `category` (id, parent, name, sort)
+values ('00000600', '00000000', '运维&测试', 600);
+insert into `category` (id, parent, name, sort)
+values ('00000601', '00000600', '运维', 601);
+insert into `category` (id, parent, name, sort)
+values ('00000602', '00000600', '自动化运维', 602);
+insert into `category` (id, parent, name, sort)
+values ('00000603', '00000600', '中间件', 603);
+insert into `category` (id, parent, name, sort)
+values ('00000604', '00000600', 'linux', 604);
+insert into `category` (id, parent, name, sort)
+values ('00000605', '00000600', '测试', 605);
+insert into `category` (id, parent, name, sort)
+values ('00000606', '00000600', '自动化测试', 606);
+insert into `category` (id, parent, name, sort)
+values ('00000700', '00000000', '数据库', 700);
+insert into `category` (id, parent, name, sort)
+values ('00000701', '00000700', 'mysql', 701);
+insert into `category` (id, parent, name, sort)
+values ('00000702', '00000700', 'redis', 702);
+insert into `category` (id, parent, name, sort)
+values ('00000703', '00000700', 'mongodb', 703);
 # 课程分类
 drop table if exists `course_category`;
 create table `course_category`
@@ -165,7 +230,8 @@ create table `teacher`
     primary key (`id`)
 ) engine = innodb
   default charset = utf8mb4 comment ='讲师';
-insert into `teacher` (id,name,nickname,image,position,motto ,intro ) values ('00000001','叶秋','叶落知秋','','Java架构师','','');
+insert into `teacher` (id, name, nickname, image, position, motto, intro)
+values ('00000001', '叶秋', '叶落知秋', '', 'Java架构师', '', '');
 
 -- 文件
 drop table if exists `file`;
@@ -201,14 +267,23 @@ create table `course_content_file`
     primary key (`id`)
 ) engine = innodb
   default charset = utf8mb4 comment ='课程内容文件';
-
+alter table `file` add column (`shard_index` int comment '已上传分片');
+alter table `file` add column (`shard_size` int comment '分片大小|B');
+alter table `file` add column (`shard_total` int comment '分片总数');
+alter table `file` add column (`key` varchar(32) comment '文件标识');
+alter table `file` add unique key key_unique (`key`);
+alter table `file` add column (`vod` char(32) comment 'vod|阿里云vod');
 -- -------------------------测试
 drop table if exists `test`;
-create table `test` (
-  `id` char(8) not null default '' comment 'id',
-  `name` varchar(50) comment '名称',
-  primary key (`id`)
-) engine=innodb default charset=utf8mb4 comment='测试';
+create table `test`
+(
+    `id`   char(8) not null default '' comment 'id',
+    `name` varchar(50) comment '名称',
+    primary key (`id`)
+) engine = innodb
+  default charset = utf8mb4 comment ='测试';
 
-insert into `test` (id, name) values (1, '测试');
-insert into `test` (id, name) values (2, '测试数据');
+insert into `test` (id, name)
+values (1, '测试');
+insert into `test` (id, name)
+values (2, '测试数据');
