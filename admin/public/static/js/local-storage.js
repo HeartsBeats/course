5e383903dcfc8a64781042779c5ae95e7cceaed7
Hearts <1127957114@qq.com>
2020-11-21 16:45:40 +0800

    1. 用户管理与登录：增加记住登录信息功能，安全加固，本地缓存保存密码密文

--- admin/public/static/js/local-storage.js
+++ admin/public/static/js/local-storage.js
@@ -1,3 +1,12 @@
+LOCAL_KEY_REMEMBER_USER = "LOCAL_KEY_REMEMBER_USER"; // 课程管理页面点击章管理时，保存课程信息
+//localStorage生命周期是永久，这意味着除非用户显示在浏览器提供的UI上清除localStorage信息，否则这些信息将永远存在。
+//
+// sessionStorage生命周期为当前窗口或标签页，一旦窗口或标签页被永久关闭了，那么所有通过sessionStorage存储的数据
+//也就被清空了。
+// 不同浏览器无法共享localStorage或sessionStorage中的信息。相同浏览器的不同页面间可以共享相同的 localStorage（
+// 页面属于相同域名和端口），
+// 但是不同页面或标签页间无法共享sessionStorage的信息。这里需要注意的是，页面及标 签页仅指顶级窗口，
+// 如果一个标签页包含多个iframe标签且他们属于同源页面，那么他们之间是可以共享sessionStorage的。
 LocalStorage = {
   get: function (key) {
     let v = localStorage.getItem(key);
--- admin/src/views/login.vue
+++ admin/src/views/login.vue
@@ -44,7 +44,7 @@
 
                         <div class="clearfix">
                           <label class="inline">
-                            <input type="checkbox" class="ace"/>
+                            <input v-model="remember" type="checkbox" class="ace"/>
                             <span class="lbl">记住我</span>
                           </label>
 
@@ -73,27 +73,59 @@
 <script>
 export default {
   name: "login",
-  data: function () {
+  data: function() {
     return {
       user: {},
+      remember: true // 默认勾选记住账号密码
     }
   },
-  mounted: function () {
+  mounted: function() {
+    let _this = this;
     $("body").removeClass("no-skin");
     $("body").attr("class", "login-layout light-login");
     // console.log("login");
+
+    // 从缓存中获取记住的用户名密码，如果获取不到，说明上一次没有勾选“记住我”
+    let rememberUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER);
+    if (rememberUser) {
+      _this.user = rememberUser;
+    }
   },
   methods: {
-    login() {
+    login () {
       let _this = this;
-      _this.user.password = hex_md5(_this.user.password + KEY);
+      // 如果密码是从缓存带出来的，则不需要重新加密,如果密码是从缓存中取出再加密就是两次加密
+      let md5 = hex_md5(_this.user.password);
+      let rememberUser = LocalStorage.get(LOCAL_KEY_REMEMBER_USER) || {};
+      if (md5 !== rememberUser.md5) {
+        _this.user.password = hex_md5(_this.user.password + KEY);
+      }
+
       Loading.show();
-      _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response) => {
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/system/admin/user/login', _this.user).then((response)=>{
         Loading.hide();
         let resp = response.data;
         if (resp.success) {
-          console.log("登录成功"+resp.content);
+          console.log("登录成功：", resp.content);
+          let loginUser = resp.content;
           Tool.setLoginUser(resp.content);
+
+          // 判断“记住我”
+          if (_this.remember) {
+            // 如果勾选记住我，则将用户名密码保存到本地缓存
+            // 新：这里保存密码密文，并保存密文md5，用于检测密码是否被重新输入过
+            let md5 = hex_md5(_this.user.password);
+            LocalStorage.set(LOCAL_KEY_REMEMBER_USER, {
+              loginName: loginUser.loginName,
+              // password: _this.user.passwordShow,
+              password: _this.user.password,
+              // 保证加密两次
+              md5: md5
+            });
+          } else {
+            // 没有勾选“记住我”时，要把本地缓存清空，否则按照mounted的逻辑，下次打开时会自动显示用户名密码
+            LocalStorage.set(LOCAL_KEY_REMEMBER_USER, null);
+          }
           _this.$router.push("/welcome")
         } else {
           Toast.warning(resp.message)
