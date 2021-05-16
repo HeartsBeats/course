acd895f84fb246c86b964b8b076ead2825b099c0
Hearts <1127957114@qq.com>
2021-05-16 17:16:41 +0800

    feat: 通用权限管理：增加后端接口权限拦截

--- gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
+++ gateway/src/main/java/com/course/gateway/filter/LoginAdminGatewayFilter.java
@@ -1,5 +1,8 @@
 package com.course.gateway.filter;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
@@ -10,6 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
+
 import javax.annotation.Resource;
 
 @Component
@@ -38,24 +42,44 @@ public class LoginAdminGatewayFilter implements GatewayFilter, Ordered {
         String token = exchange.getRequest().getHeaders().getFirst("token");
         LOG.info("控台登录验证开始，token：{}", token);
         if (token == null || token.isEmpty()) {
-            LOG.info( "token为空，请求被拦截" );
+            LOG.info("token为空，请求被拦截");
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         }
         Object object = redisTemplate.opsForValue().get(token);
         if (object == null) {
-            LOG.warn( "token无效，请求被拦截" );
+            LOG.warn("token无效，请求被拦截");
             exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
             return exchange.getResponse().setComplete();
         } else {
             LOG.info("已登录：{}", object);
+            // 增加权限校验，gateway里没有LoginUserDto，所以全部用JSON操作
+            LOG.info("接口权限校验，请求地址：{}", path);
+            boolean exist = false;
+            JSONObject loginUserDto = JSON.parseObject(String.valueOf(object));
+            JSONArray requests = loginUserDto.getJSONArray("requests");
+            // 遍历所有【权限请求】，判断当前请求的地址是否在【权限请求】里
+            for (int i = 0, l = requests.size(); i < l; i++) {
+                String request = (String) requests.get(i);
+                if (path.contains(request)) {
+                    exist = true;
+                    break;
+                }
+            }
+            if (exist) {
+                LOG.info("权限校验通过");
+            } else {
+                LOG.warn("权限校验未通过");
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+
             return chain.filter(exchange);
         }
     }
 
     @Override
-    public int getOrder()
-    {
+    public int getOrder() {
         return 1;
     }
 }
