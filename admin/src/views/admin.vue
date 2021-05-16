fb3d84eba217a4086060dd88f42b950fb875fd7c
Hearts <1127957114@qq.com>
2021-05-16 17:02:41 +0800

    feat: 通用权限管理：前端界面权限拦截，完成所有界面控制

--- admin/src/views/admin.vue
+++ admin/src/views/admin.vue
@@ -404,7 +404,7 @@
           </li>
 
           <li class="">
-            <a href="#" class="dropdown-toggle">
+            <a v-show="hasResource('02')" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
               <span class="menu-text"> 业务管理 </span>
 
@@ -414,7 +414,7 @@
             <b class="arrow"></b>
 
             <ul class="submenu">
-              <li class="" id="business-category-sidebar">
+              <li v-show="hasResource('0201')" id="business-category-sidebar">
                 <router-link to="/business/category">
                   <i class="menu-icon fa fa-caret-right"></i>
                   分类管理
@@ -422,7 +422,7 @@
 
                 <b class="arrow"></b>
               </li>
-              <li class="" id="business-course-sidebar">
+              <li v-show="hasResource('0202')" id="business-course-sidebar">
                 <router-link to="/business/course">
                   <i class="menu-icon fa fa-caret-right"></i>
                   课程管理
@@ -430,7 +430,7 @@
 
                 <b class="arrow"></b>
               </li>
-              <li class="" id="business-teacher-sidebar">
+              <li v-show="hasResource('0203')" id="business-teacher-sidebar">
                 <router-link to="/business/teacher">
                   <i class="menu-icon fa fa-caret-right"></i>
                   讲师管理
@@ -443,7 +443,7 @@
           </li>
 
           <li class="">
-            <a href="#" class="dropdown-toggle">
+            <a v-show="hasResource('03')" class="dropdown-toggle">
               <i class="menu-icon fa fa-list"></i>
               <span class="menu-text"> 文件管理 </span>
 
@@ -453,7 +453,7 @@
             <b class="arrow"></b>
 
             <ul class="submenu">
-              <li class="" id="file-file-sidebar">
+              <li v-show="hasResource('0301')" id="file-file-sidebar">
                 <router-link to="/file/file">
                   <i class="menu-icon fa fa-caret-right"></i>
                   文件管理
