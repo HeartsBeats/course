f9ee1c5f5e32a00bbca253e862d8b00abc381674
Hearts <1127957114@qq.com>
2021-05-22 14:46:30 +0800

    feat: 课程列表页面开发，加载所有分类

--- web/src/main.js
+++ web/src/main.js
@@ -6,6 +6,23 @@ import axios from 'axios'
 Vue.config.productionTip = false;
 Vue.prototype.$ajax = axios;
 
+// 解决每次ajax请求，对应的sessionId不一致的问题
+axios.defaults.withCredentials = true;
+
+/**
+ * axios拦截器
+ */
+axios.interceptors.request.use(function (config) {
+  console.log("请求：", config);
+  return config;
+}, error => {
+});
+axios.interceptors.response.use(function (response) {
+  console.log("返回结果：", response);
+  return response;
+}, error => {
+});
+
 // 全局过滤器
 Object.keys(filter).forEach(key => {
   Vue.filter(key, filter[key])
--- web/src/views/list.vue
+++ web/src/views/list.vue
@@ -1,9 +1,15 @@
 <template>
   <main role="main">
-    <div class="album py-5 bg-light">
-      <div class="container">
-        <div class="row">
-          <div v-for="o in courses" class="col-md-4">
+    <div className="album py-5 bg-light">
+      <div className="container">
+        <div className="row">
+          <div className="col-md-12">
+            <pagination ref="pagination" v-bind:list="listCourse"></pagination>
+          </div>
+        </div>
+        <br>
+        <div className="row">
+          <div v-for="o in courses" className="col-md-4">
             <the-course v-bind:course="o"></the-course>
           </div>
           <h3 v-show="courses.length === 0">课程还未上架</h3>
@@ -16,18 +22,23 @@
 
 <script>
 import TheCourse from "../components/the-course";
+import Pagination from "../components/pagination";
 
 export default {
-  components: {TheCourse},
+  components: {Pagination, TheCourse},
   name: 'list',
   data: function () {
     return {
       courses: [],
+      level1: [],
+      level2: [],
     }
   },
   mounted() {
     let _this = this;
-    _this.listCourse();
+    _this.$refs.pagination.size = 1;
+    _this.listCourse(1);
+    _this.allCategory();
   },
   methods: {
     /**
@@ -37,14 +48,46 @@ export default {
       let _this = this;
       _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/course/list', {
         page: page,
-        size: 3,
+        size: _this.$refs.pagination.size,
       }).then((response) => {
         let resp = response.data;
         if (resp.success) {
           _this.courses = resp.content.list;
+          _this.$refs.pagination.render(page, resp.content.total);
         }
       }).catch((response) => {
         console.log("error：", response);
+      })
+    },
+
+    /**
+     * 所有分类查询
+     */
+    allCategory() {
+      let _this = this;
+      _this.$ajax.post(process.env.VUE_APP_SERVER + '/business/web/category/all').then((response) => {
+        let resp = response.data;
+        let categorys = resp.content;
+
+        // 将所有记录格式化成树形结构
+        _this.level1 = [];
+        for (let i = 0; i < categorys.length; i++) {
+          let c = categorys[i];
+          if (c.parent === '00000000') {
+            _this.level1.push(c);
+            for (let j = 0; j < categorys.length; j++) {
+              let child = categorys[j];
+              if (child.parent === c.id) {
+                if (Tool.isEmpty(c.children)) {
+                  c.children = [];
+                }
+                c.children.push(child);
+              }
+            }
+          }
+        }
+
+        _this.level2 = [];
       })
     },
 
