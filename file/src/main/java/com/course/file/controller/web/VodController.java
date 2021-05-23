ad0598be448d625cc964b4e3c79ca24771d079f6
Hearts <1127957114@qq.com>
2021-05-23 15:26:00 +0800

    feat: 课程详情页面开发，增加视频播放功能

---
+++ file/src/main/java/com/course/file/controller/web/VodController.java
@@ -0,0 +1,9 @@
+/**
+ * @ProjectName: course
+ * @Package: com.course.business.controller.web
+ * @ClassName: VodController
+ * @Author: 游佳琪
+ * @Description:
+ * @Date: 2021-5-23 15:23
+ * @Version: 1.0
+ */
