3da75ab8892e48d773874ea911d60c522f67aed4
Hearts <1127957114@qq.com>
2021-05-25 10:07:12 +0800

    feat: 控台欢迎页面开发，增加顶部统计

--- admin/src/views/admin/welcome.vue
+++ admin/src/views/admin/welcome.vue
@@ -1,26 +1,68 @@
 <template>
   <div>
-    <span>HELLO WELCOME CONTROLLER !</span>
+    <div class="space-12"></div>
+    <div class="row">
+      <div class="col-xs-12">
+        <div class="center">
+          <span class="btn btn-app btn-sm btn-light no-hover">
+              <span class="line-height-1 bigger-170 blue"> 518 </span>
+              <br>
+              <span class="line-height-1 smaller-90"> 课程 </span>
+          </span>
+          <span class="btn btn-app btn-sm btn-yellow no-hover">
+              <span class="line-height-1 bigger-170"> 6,321 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 大章 </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-pink no-hover">
+              <span class="line-height-1 bigger-170"> 4,182 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 小节 </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-success no-hover">
+              <span class="line-height-1 bigger-170"> 5.23 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 会员(万) </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-primary no-hover">
+              <span class="line-height-1 bigger-170"> 12.76 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 报名(万) </span>
+          </span>
+
+          <span class="btn btn-app btn-sm btn-grey no-hover">
+              <span class="line-height-1 bigger-170"> 32.96 </span>
+
+              <br>
+              <span class="line-height-1 smaller-90"> 评论(万) </span>
+          </span>
+
+        </div>
+
+        <div class="space-12"></div>
+
+
+      </div>
+
+
+    </div><!-- /.row -->
   </div>
 </template>
-<!--<script>-->
-<!--export default {-->
-<!--  name: "welcome",-->
-<!--  mounted: function () {-->
-<!--    this.$parent.activeSidebar("welcome-sidebar")-->
-<!--  },-->
-<!--  methods: {-->
 
-<!--    }-->
-<!--  }-->
-<!--</script>-->
 <script>
 export default {
   name: "welcome",
-  mounted: function() {
+  mounted: function () {
+    // sidebar激活样式方法一
     // this.$parent.activeSidebar("welcome-sidebar");
   },
-  methods: {
-  }
+  methods: {}
 }
 </script>
